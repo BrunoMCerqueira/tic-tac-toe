a6825b04e083546f2180e89dccf316065219bf62
Bruno Cerqueira <brunocerqueiram@gmail.com>
2019-04-21 23:58:50 -0300

    Style start game button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import WrapSquares from './components/WrapSquares';
+import "./styles.css";
 
 class App extends Component {
   render() {
--- src/components/StartGame/index.js
+++ src/components/StartGame/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import "./styles.css";
 
 const StartGame = () => (
-  <button>Start Game</button>
+  <button className="start-design">Start Game</button>
 );
 
 export default StartGame;

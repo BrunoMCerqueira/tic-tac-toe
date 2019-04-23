519c30d863747880fe6ffd54ef8656b2b52020af
Bruno Cerqueira <brunocerqueiram@gmail.com>
2019-04-23 01:03:42 -0300

    Style history elements and adjust squares sizes.

--- src/components/HistoryPlays/index.js
+++ src/components/HistoryPlays/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import "./styles.css";
 
 class HistoryPlays extends Component {
   render() {
     const { history } = this.props;
     return (
-      <ol>
-        {history && history.map((history, index) => <li key={index}>exemplo</li>)}
+      <ol className="histories-list">
+        {history && history.map((history, index) => <li className="history-line" key={index}>Jogada {index + 1}</li>)}
       </ol>
     )
   }
